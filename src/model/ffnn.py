d1d42a8f584ca8e99654082e5f4e9af1998de6c2
ValentinoTriadi <13522164@std.stei.itb.ac.id>
2025-03-28 10:03:18 +0700

    refactor: refactor ffnn model

--- src/model/ffnn.py
+++ src/model/ffnn.py
@@ -1,61 +1,150 @@
-import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import concurrent.futures
+from typing import Literal
 from utils.activationFunction import ActivationFunction
 from utils.lossFunction import LossFunction
-from utils.weightInitiation import WeightInitiation
-import time
+from utils.weightInitiation import WeightInitiation, WeightInitiationMethod
+from view.gui import GUI, GraphModel
+from PyQt6.QtWidgets import QApplication
+import time, sys, pickle
 
 
-class FFNN2:
+class FFNN:
+    """
+    Kelas untuk menampung model Feed Forward Neural Network
+
+    @param jumlah_neuron: list[int]
+        Jumlah neuron pada tiap layer (termasuk input dan output)
+    @param fungsi_aktivasi: list[str]
+        Fungsi aktivasi pada tiap layer
+        ['Linear', 'Sigmoid', 'ReLU', 'Tanh', 'Softmax']
+    @param fungsi_loss: str
+        Fungsi loss yang digunakan
+        ['MSE', 'BinaryCrossEntropy', 'CategoricalCrossEntropy']
+    @param inisialisasi_bobot: str
+        Metode inisialisasi bobot
+        ['zero', 'uniform', 'normal', 'xavier-uniform', 'xavier-normal']
+    @param verbose: int
+        Menampilkan log training
+        0 = Tidak menampilkan apa-apa
+        1 = Menampilkan progress bar
+
+    Optional Param:
+    -- Weight Initiation -> uniform --
+    @param lower_bound: float
+        Batas bawah nilai random
+    @param upper_bound: float
+        Batas atas nilai random
+    @param seed: int
+        Seed untuk reproducibility
+
+    -- Weight Initiation -> normal --
+    @param mean: float
+        Mean dari distribusi normal
+    @param std: float
+        Standar deviasi dari distribusi normal
+    @param seed: int
+        Seed untuk reproducibility
+    """
+
     def __init__(
         self,
-        jumlah_neuron,
-        fungsi_aktivasi,
-        fungsi_loss,
-        inisialisasi_bobot,
-        verbose=0,
-        lower_bound=-1.0,
-        upper_bound=1.0,
-        mean=0.0,
-        std=1.0,
-        seed=0,
+        jumlah_neuron: list[int],
+        fungsi_aktivasi: list[Literal["Linear", "Sigmoid", "ReLU", "Tanh", "Softmax"]],
+        fungsi_loss: Literal["MSE", "BinaryCrossEntropy", "CategoricalCrossEntropy"],
+        inisialisasi_bobot: Literal[
+            "zero", "uniform", "normal", "xavier-uniform", "xavier-normal"
+        ],
+        lower_bound: float = -1.0,
+        upper_bound: float = 1.0,
+        mean: float = 0.0,
+        std: float = 1.0,
+        seed: int = 0,
+        verbose: int = 0,
     ):
-        self.verbose = verbose
         self.jumlah_neuron = jumlah_neuron
         self.jumlah_layer = len(jumlah_neuron) - 1
-        self.fungsi_aktivasi = [
-            ActivationFunction([method]).get_activation_function(method)
-            for method in fungsi_aktivasi
-        ]
 
-        self.fungsi_loss_class = LossFunction(fungsi_loss)
-        self.fungsi_loss = self.fungsi_loss_class.get_lost_function()
+        # Inisialisasi fungsi aktivasi
+        self.fungsi_aktivasi_str = fungsi_aktivasi
+        self.fungsi_aktivasi = list(
+            ActivationFunction(fungsi_aktivasi).get_batch_activation_function()
+        )
+
+        # Inisialisasi fungsi loss
         self.fungsi_loss_str = fungsi_loss
+        self.fungsi_loss_class = LossFunction(self.fungsi_loss_str)
+        self.fungsi_loss = self.fungsi_loss_class.get_lost_function()
 
+        # Inisialisasi bobot
+        # ? Bias dimasukkan ke dalam bobot indeks terakhir
         self.inisialisasi_bobot_str = inisialisasi_bobot
         self.inisialisasi_bobot = WeightInitiation(
             inisialisasi_bobot, self.jumlah_layer, jumlah_neuron
         )
-
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.mean = mean
+        self.std = std
+        self.seed = seed
         self.init_bobot()
-        self.fungsi_aktivasi_str = fungsi_aktivasi
+
+        self.X = None
+        self.y = None
+        self.hasil = None
+        self.loss = None
+        self.verbose = verbose
+        self.weight_history = []
 
     def init_bobot(self):
-        self.bobot = self.inisialisasi_bobot.init_weights()
-        # for i in range(self.jumlah_layer):
-        # print(f"Shape bobot layer {i}: {self.bobot[i].shape}")
+        if self.inisialisasi_bobot_str == WeightInitiationMethod.ZERO.value:
+            self.bobot = self.inisialisasi_bobot.init_weights()
+        elif self.inisialisasi_bobot_str == WeightInitiationMethod.UNIFORM.value:
+            self.bobot = self.inisialisasi_bobot.init_weights(
+                low=self.lower_bound,
+                high=self.upper_bound,
+                seed=self.seed,
+            )
+        elif self.inisialisasi_bobot_str == WeightInitiationMethod.NORMAL.value:
+            self.bobot = self.inisialisasi_bobot.init_weights(
+                mean=self.mean,
+                std=self.std,
+                seed=self.seed,
+            )
+        elif self.inisialisasi_bobot_str == WeightInitiationMethod.XAVIER_UNIFORM.value:
+            self.bobot = self.inisialisasi_bobot.init_weights(
+                seed=self.seed,
+            )
+        elif self.inisialisasi_bobot_str == WeightInitiationMethod.XAVIER_NORMAL.value:
+            self.bobot = self.inisialisasi_bobot.init_weights(
+                seed=self.seed,
+            )
+        else:
+            raise ValueError("Metode inisialisasi bobot tidak valid")
 
-    def forward(self, X):
+    def forward(self, X: np.ndarray):
+        """
+        Melakukan feed forward pada model
+
+        @param X: np.ndarray
+            Input data
+        @return: np.ndarray
+            Output dari model
+        """
         hasil = [X]
         for i in range(self.jumlah_layer):
-            X_with_bias = np.hstack([np.ones((hasil[-1].shape[0], 1)), hasil[-1]])
+            X_with_bias = np.clip(
+                np.hstack([np.ones((hasil[-1].shape[0], 1)), hasil[-1]]), 0.000001, None
+            )
             Z = np.dot(X_with_bias, self.bobot[i])
             hasil.append(self.fungsi_aktivasi[i](Z))
         return hasil
 
-    def backward(self, hasil, y):
+    def backward(self, hasil: np.ndarray, y: np.ndarray):
+        """
+        Melakukan backpropagation pada model.
+        """
         deltas = [None] * self.jumlah_layer
         loss_derivative = self.fungsi_loss_class.get_loss_derivative()
         activation_derivative = ActivationFunction(
@@ -75,14 +164,15 @@ class FFNN2:
                 self.fungsi_aktivasi_str
             ).get_activation_derivative(self.fungsi_aktivasi_str[i])
 
-            upstream_gradient = deltas[i + 1] @ self.bobot[i + 1][1:].T  
+            upstream_gradient = deltas[i + 1] @ self.bobot[i + 1][1:].T
 
             if self.fungsi_aktivasi_str[i] == "Softmax":
                 # Compute delta = (upstream_gradient) @ Jacobian
-                deltas[i] = activation_derivative_i(hasil[i + 1], grad_loss=upstream_gradient)
+                deltas[i] = activation_derivative_i(
+                    hasil[i + 1], grad_loss=upstream_gradient
+                )
             else:
                 deltas[i] = upstream_gradient * activation_derivative_i(hasil[i + 1])
-
 
         gradients = []
         for i in range(self.jumlah_layer):
@@ -90,15 +180,37 @@ class FFNN2:
             gradients.append((X_with_bias.T @ deltas[i]) / hasil[i].shape[0])
         return gradients
 
-    def update(self, gradients, lr):
+    def update(self, gradients):
+        """
+        Melakukan update bobot berdasarkan gradien yang dihitung
+        """
         for i in range(self.jumlah_layer):
-            self.bobot[i] -= lr * gradients[i]
+            self.bobot[i] -= self.lr * gradients[i]
 
-    def fit(self, X, y, batch, lr, epochs):
+    def fit(self, X: np.ndarray, y: np.ndarray, batch: int, lr: float, epochs: int):
+        """
+        Melatih model dengan data latih
+        @param X: np.ndarray
+            Data latih
+        @param y: np.ndarray
+            Target data latih
+        @param batch: int
+            Ukuran batch
+        @param lr: float
+            Learning rate
+        @param epochs: int
+            Jumlah epoch
+        """
+        self.X = X
+        self.y = y
+        self.lr = lr
+        self.batch = batch
+
         num_samples = X.shape[0]
         now = time.time()
 
         for epoch in range(epochs):
+            # Shuffle data
             indices = np.random.permutation(num_samples)
             X_shuffled = X[indices]
             y_shuffled = y[indices]
@@ -121,7 +233,7 @@ class FFNN2:
                     self.gradients = gradients
 
                     # Update bobot
-                    self.update(gradients, lr)
+                    self.update(gradients)
 
             # loss per epoch
             hasil_final = self.forward(X)
@@ -135,7 +247,14 @@ class FFNN2:
         if self.verbose == 1:
             print(f"\nLoss: {loss:.6f}")
 
-    def predict(self, X):
+    def predict(self, X: np.ndarray):
+        """
+        Melakukan prediksi dengan model
+        @param X: np.ndarray
+            Data input untuk prediksi
+        @return: np.ndarray
+            Hasil prediksi
+        """
         return np.argmax(self.forward(X)[-1], axis=1)
 
     def save_model_pickle(self, filename: str):
@@ -244,3 +363,16 @@ class FFNN2:
 
         plt.tight_layout()
         plt.show()
+
+    def model_visualize(self):
+        """
+        Menampilkan visualisasi model
+        """
+        graph_model = GraphModel(self.jumlah_neuron, self.bobot, self.gradients)
+        layer_distribution_input = [0, 1, 2, 3]
+        app = QApplication.instance()
+        if app is None:
+            app = QApplication(sys.argv)
+        gui = GUI(graph_model, layer_distribution_input)
+        return gui
+        # gui.show()
